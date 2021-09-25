44a8023e4b4cca31014a8aa0e9bfce4d18c88bb5
flynnn555 <geoffarmstrong555@gmail.com>
2021-09-25 10:42:41 +0100

    copy code from desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular for absolute beginners'
+  checkboxLabel = " I agree your terms";
+  buttonClickedMessage = "You clicked the button";
+  buttonClicked = false;
+
+  checkboxState = false;
+  checkboxCheckedMessage = "You checked the check box"
+  userAgreedTerms = false;
+  userAgreedTermsMessage = "You agreed our terms"
+  userNotAgreedTerms = false;
+  userNotAgreedTermsMessage = "You did not agree our terms"
+
+  clicked(): void {
+    this.resetFunction()
+    // console.log("you clicked me");
+    if (this.checkboxState == true) {
+      this.userAgreedTerms = true;
+    }
+    else {
+      this.userAgreedTerms = false;
+      this.userNotAgreedTerms = true;
+    }
+    this.buttonClicked = true;
+  }
+
+  resetFunction(): void {
+    this.buttonClicked = false;
+    this.checkboxState = false;
+    this.userAgreedTerms = false;
+    this.userNotAgreedTerms
+  }
+  
+  checkboxEvent(): void {
+
+    // this.checkboxChecked = true;
+    this.checkboxState = this.checkboxState ? false : true;
+  }
+
 }
+
